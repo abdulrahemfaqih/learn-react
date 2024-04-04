b68e249b4383217ba03dff58fe3945580f04dc80
abdulrahemfaqih <faqih3935@gmail.com>
2024-04-04 22:26:05 +0700

    initial init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+
+   return (
+      <div className="font-poppins flex justify-center gap-2 bg-blue-900 items-center min-h-screen">
+         <button
+            type="button"
+            className="p-4 bg-black text-white rounded-md">Click Me!
+         </button>
+         <button
+            type="button"
+            className="p-4 bg-black text-white rounded-md">Click Me!
+         </button>
+         <button
+            type="button"
+            className="p-4 bg-black text-white rounded-md">Click Me!
+         </button>
+      </div>
+   );
+}
+
+export default App;
